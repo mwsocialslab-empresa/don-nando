b8b3ebbad7ef78ad8e28351b0663a4ea3854f5db
walterG <waltergalelab@gmail.com>
2026-02-05 17:26:22 -0300

    probando este link /link.mercadopago.com.ar/home/iniciarsesion

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ function enviarPedidoWhatsApp() {
     const cvuMP = "0000003100047275611542";
     
     // Este link intenta forzar la apertura de la APP de Mercado Pago directamente
-    const linkApp = "link.mercadopago.com.ar/home"; 
+    const linkApp = "/link.mercadopago.com.ar/home/iniciarsesion"; 
     // Link de respaldo web simple
     //const linkWeb = "https://www.mercadopago.com.ar/home";
 
